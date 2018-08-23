a53b6c2a1bf7b8c984448a453a16d822ecd12190
joewalsh <joewalsh@users.noreply.github.com>
2018-08-23 14:39:56 -0400

    add stack view above toolbar

--- WMF Framework/Toolbar.swift
+++ WMF Framework/Toolbar.swift
@@ -3,19 +3,27 @@ import UIKit
 @objc(WMFToolbar)
 class Toolbar: SetupView {
     private let toolbar: UIToolbar = UIToolbar()
+    private let stackView: UIStackView = UIStackView()
     
     override func setup() {
         super.setup()
         
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         toolbar.translatesAutoresizingMaskIntoConstraints = false
-
+        
+        addSubview(stackView)
         addSubview(toolbar)
-
-        let toolbarTop = topAnchor.constraint(equalTo: toolbar.topAnchor)
+        
+        let svTop = topAnchor.constraint(equalTo: stackView.topAnchor)
+        let svLeading = leadingAnchor.constraint(equalTo: leadingAnchor)
+        let svTrailing = trailingAnchor.constraint(equalTo: trailingAnchor)
+        
+        let toolbarTop = stackView.bottomAnchor.constraint(equalTo: toolbar.topAnchor)
         let toolbarBottom = toolbar.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
         let toolbarLeading = leadingAnchor.constraint(equalTo: toolbar.leadingAnchor)
         let toolbarTrailing = toolbar.trailingAnchor.constraint(equalTo: trailingAnchor)
-        addConstraints([toolbarTop, toolbarBottom, toolbarLeading, toolbarTrailing])
+        
+        addConstraints([svTop, svLeading, svTrailing, toolbarTop, toolbarBottom, toolbarLeading, toolbarTrailing])
     }
     
     @objc var items: [UIBarButtonItem]? {
