03c8dd5466b14837532c8eb180cfb76da6e0bc89
joewalsh <joewalsh@users.noreply.github.com>
2018-08-23 13:06:40 -0400

    use safeAreaLayoutGuide so that the toolbar extends below the home indicator

--- WMF Framework/Toolbar.swift
+++ WMF Framework/Toolbar.swift
@@ -3,28 +3,19 @@ import UIKit
 @objc(WMFToolbar)
 class Toolbar: SetupView {
     private let toolbar: UIToolbar = UIToolbar()
-    private let homeIndicatorUnderlayView: UIView = UIView()
     
     override func setup() {
         super.setup()
         
         toolbar.translatesAutoresizingMaskIntoConstraints = false
-        homeIndicatorUnderlayView.translatesAutoresizingMaskIntoConstraints = false
 
         addSubview(toolbar)
-        addSubview(homeIndicatorUnderlayView)
 
         let toolbarTop = topAnchor.constraint(equalTo: toolbar.topAnchor)
-        let toolbarBottom = toolbar.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor)
+        let toolbarBottom = toolbar.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
         let toolbarLeading = leadingAnchor.constraint(equalTo: toolbar.leadingAnchor)
         let toolbarTrailing = toolbar.trailingAnchor.constraint(equalTo: trailingAnchor)
-        
-        let underlayTop = layoutMarginsGuide.bottomAnchor.constraint(equalTo: homeIndicatorUnderlayView.topAnchor)
-        let underlayBottom = homeIndicatorUnderlayView.bottomAnchor.constraint(equalTo: bottomAnchor)
-        let underlayLeading = leadingAnchor.constraint(equalTo: homeIndicatorUnderlayView.leadingAnchor)
-        let underlayTrailing = homeIndicatorUnderlayView.trailingAnchor.constraint(equalTo: trailingAnchor)
-        
-        addConstraints([toolbarTop, toolbarBottom, toolbarLeading, toolbarTrailing, underlayTop, underlayBottom, underlayLeading, underlayTrailing])
+        addConstraints([toolbarTop, toolbarBottom, toolbarLeading, toolbarTrailing])
     }
     
     @objc var items: [UIBarButtonItem]? {
@@ -43,6 +34,5 @@ extension Toolbar: Themeable {
     public func apply(theme: Theme) {
         toolbar.barTintColor = theme.colors.chromeBackground
         toolbar.isTranslucent = false
-        homeIndicatorUnderlayView.backgroundColor = theme.colors.chromeBackground
     }
 }
